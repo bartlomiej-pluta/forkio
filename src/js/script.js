f463a057265c20d2f85754bcb67efeb4ca7fc150
Bartlomiej Pluta (PGS Software) <bpluta@pgs-soft.com>
2018-03-15 17:07:38 +0100

    Enable navbar shrinking on scrolling

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello, world');
+$(document).ready(function () {
+  var scrollTop = 0;
+
+  $(window).scroll(function () {
+    scrollTop = $(window).scrollTop();
+
+    if (scrollTop >= 100) {
+      $('#nav').addClass('shrink');
+    } else if (scrollTop < 100) {
+      $('#nav').removeClass('shrink');
+    }
+  });
+});
